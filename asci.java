0ec11a711d7030c875a1aada130549e567c9587e
Anmol <anmol.k1420@gmail.com>
2025-07-29 17:25:35 +0530

    Add files via upload

---
+++ asci.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ascii
+{
+    public static void main(String args[])
+    {
+        Scanner Sc=new Scanner(System.in);
+        char c=Sc.nextLine().charAt(0);
+        int a=c;
+        char s=(char)a;
+        System.out.println(a+" "+s);
+    }
+}
